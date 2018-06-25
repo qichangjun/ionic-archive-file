93b363ba015b38a000db49323c1553b93eae852f
qichangjun <1061427304@qq.com>
2018-06-25 18:12:08 +0800

    remove debug code

--- src/pages/main-page/search-main/search-main.ts
+++ src/pages/main-page/search-main/search-main.ts
@@ -27,15 +27,7 @@ export class searchMain {
         private loadingCtrl: LoadingController,
         public modalCtrl: ModalController
     ) {      
-        let preview = this.modalCtrl.create(
-            previewPDF
-            // PreviewDocModal
-           );
-        preview.present();
-        preview.onDidDismiss(data => {
-            console.log(data);
-        });      
-        // this.getList()
+        this.getList()
     }
 
     /**
