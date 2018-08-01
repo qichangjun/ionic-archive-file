3aec9e2be7a0165c5645ed550625095c86751a88
qichangjun <1061427304@qq.com>
2018-08-01 13:51:05 +0800

    search fn

--- src/core/service/baseConfig.service.ts
+++ src/core/service/baseConfig.service.ts
@@ -7,6 +7,7 @@ export class baseConfig {
     // private baseUrl = 'http://10.150.156.40:6233/app'
     //debug
     // private baseUrl = '/appapi'
+    //local-prod
     private baseUrl = 'http://192.168.0.153:6233/app'
     getBaseUrl(){
         return this.baseUrl
