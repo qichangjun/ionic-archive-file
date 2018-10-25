dd1f446ca9383469592862e99274e1049ae04e88
qichangjun <1061427304@qq.com>
2018-10-25 11:33:26 +0800

    preview

--- src/pages/main-page/search-main/search-main.service.ts
+++ src/pages/main-page/search-main/search-main.service.ts
@@ -145,4 +145,54 @@ export class SearchMainService {
                 this._httpHanldeService.handleError(error)
             );
     }
+
+    async getPdfPreviewPath(info): Promise<any> {        
+        let params = new URLSearchParams();
+        let userInfo = await this._storageInfoService.getAuthInfo()                
+        params.set('accessToken', userInfo.accessToken)
+        params.set('locale','zh_CN')
+        for(let key in info){
+            params.set(key,info[key])
+        }
+        return this.http.get(this._baseConfig.getBaseUrl() + this._ApiUrlService['getPdfPreviewPath'],{search:params})
+            .toPromise()
+            .then(res =>
+                this._httpHanldeService.extractData(res)
+            )
+            .catch(error =>
+                this._httpHanldeService.handleError(error)
+            );
+    }
+
+    async getElectronicRecord(id): Promise<any> {        
+        let params = new URLSearchParams();
+        let userInfo = await this._storageInfoService.getAuthInfo()                
+        params.set('accessToken', userInfo.accessToken)
+        params.set('locale','zh_CN')
+        params.set('fileId',id)
+        return this.http.get(this._baseConfig.getBaseUrl() + this._ApiUrlService['getElectronicRecord'],{search:params})
+            .toPromise()
+            .then(res =>
+                this._httpHanldeService.extractData(res)
+            )
+            .catch(error =>
+                this._httpHanldeService.handleError(error)
+            );
+    }
+
+    async getEleId(id): Promise<any> {        
+        let params = new URLSearchParams();
+        let userInfo = await this._storageInfoService.getAuthInfo()                
+        params.set('accessToken', userInfo.accessToken)
+        params.set('locale','zh_CN')
+        params.set('id',id)
+        return this.http.get(this._baseConfig.getBaseUrl() + this._ApiUrlService['getEleId'],{search:params})
+            .toPromise()
+            .then(res =>
+                this._httpHanldeService.extractData(res)
+            )
+            .catch(error =>
+                this._httpHanldeService.handleError(error)
+            );
+    }
 }
